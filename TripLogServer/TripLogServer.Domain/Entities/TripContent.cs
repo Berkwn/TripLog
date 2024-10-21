26696ecdec526230a2cea71c70d93df62698d74a
Berkwn <berkand180@gmail.com>
2024-10-21 12:55:32 +0300

    Entites Created.

---
+++ TripLogServer/TripLogServer.Domain/Entities/TripContent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripLogServer.Domain.Entities
+{
+    public sealed class TripContent
+    {
+        public TripContent()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string Title { get; set; }=string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+
+    }
+}
